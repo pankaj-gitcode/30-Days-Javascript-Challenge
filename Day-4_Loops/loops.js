676b04ea3cf59c58e37d81293aa420ba6161ee23
pankaj-gitcode <pankajaditya75@gmail.com>
2024-07-17 20:22:44 +0530

    LOOP: +While_Loop

--- Day-4_Loops/loops.js
+++ Day-4_Loops/loops.js
@@ -11,18 +11,33 @@ const print1To100 = ()=>{
 // print1To100()
 
 // . Task 2: Write a program to print the multiplication table of 5 using a for loop.
-const tableOf5 = ()=>{
-    console.log("\nMultiplication of 5:");
+const multiplicationTableOf5 = ()=>{
+    console.log("\nMultiplication table of 5:");
     for(let i=1; i<=10; i++){
         const res = 5 * i ;
         console.log(`5 x ${i} = ${res}`);
     }
 }
-tableOf5();
+// multiplicationTableOf5();
 
 
 // -------------------- Activity 2: While Loop ------------------------
 
 // . Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+let gamePlayed = true;
+let i = 1;
+let sum = 0;
+while(gamePlayed){
+    if(i< 11){
+        sum = sum + i;
+        i++;
+    }
+    else gamePlayed = false;
+    console.log(sum)
+}
+console.log([i,sum])
+
+
+
 
 // . Task 4: Write a program to print numbers from 10 to 1 using a while loop.
