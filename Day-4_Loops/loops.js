5839133ddab383a6d11f40056854c023a80c0c2d
pankaj-gitcode <pankajaditya75@gmail.com>
2024-07-18 21:59:44 +0530

    +pattern

--- Day-4_Loops/loops.js
+++ Day-4_Loops/loops.js
@@ -33,11 +33,66 @@ while(gamePlayed){
         i++;
     }
     else gamePlayed = false;
-    console.log(sum)
+    // console.log(sum)
 }
 console.log([i,sum])
 
 
-
-
 // . Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+let gamePlaying = true;
+let num = 10;
+while(gamePlaying){
+    if(num>0){
+        // console.log(num);
+        num--;
+    }
+    else gamePlaying = false;
+}
+
+
+// -------------------- Activity 4: Nested Loops --------------------------
+
+// . Task 7: Write a program to print a pattern using nested for loops:
+let star = '*';
+const rightAngledTriangle = ()=>{
+
+    for(let i=1; i<6; i++){
+        let str = '';
+        
+        for(let j=1; j<=i; j++){
+            str += star
+        }
+        // console.log("\n")
+        
+        console.log(str)
+        // return str;
+    }
+}
+// console.log(rightAngledTriangle())
+
+function printPyramid(n) {
+    for (let i = 1; i <= n; i++) {
+      let str = '';
+      for (let j = 1; j <= n - i; j++) {
+        str += ' '; // Adding spaces
+      }
+      for (let k = 0; k < 2 * i - 1; k++) {
+        str += '*'; // Adding stars
+      }
+      console.log(str);
+    }
+  }
+  
+  printPyramid(5);
+  
+
+
+
+
+
+
+// -------------------- Activity 5: Loop Control Statements -----------------
+
+// . Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue statement.
+
+// . Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
