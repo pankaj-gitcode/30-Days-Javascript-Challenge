41a6a61d375a3df9d696f3fd47fdd426c7f2e6da
pankaj-gitcode <pankajaditya75@gmail.com>
2024-07-21 01:34:20 +0530

    +continue_loop

--- Day-4_Loops/loops.js
+++ Day-4_Loops/loops.js
@@ -83,7 +83,7 @@ const factorial = (num)=>{
     }while(num>=1);
         console.log(fact)
 }
-console.log(`Factorial of ${factorial(5)}`)
+// console.log(`Factorial of ${factorial(5)}`)
 
 
 // -------------------- Activity 4: Nested Loops --------------------------
@@ -92,10 +92,10 @@ console.log(`Factorial of ${factorial(5)}`)
 let star = '*';
 const rightAngledTriangle = ()=>{
 
-    for(let i=1; i<6; i++){
+    for(let row=1; row<6; row++){
         let str = '';
         
-        for(let j=1; j<=i; j++){
+        for(let j=1; j<=row; j++){
             str += star
         }
         // console.log("\n")
@@ -109,7 +109,15 @@ const rightAngledTriangle = ()=>{
 // -------------------- Activity 5: Loop Control Statements -------------------------
 
 // . Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue statement.
-
+    const skipFunc = ()=>{
+        for (i=1; i<=10; i++){
+            if(i==5){
+                continue;
+            }
+            console.log(i)
+        }
+    }
+    skipFunc();
 
 // . Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
 
