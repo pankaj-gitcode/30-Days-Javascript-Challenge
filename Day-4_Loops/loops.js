d9fe129785edc644b7e91e5f783050718704aa7e
pankaj-gitcode <pankajaditya75@gmail.com>
2024-07-21 01:42:38 +0530

    +break_statement-Control-Loop-DONE

--- Day-4_Loops/loops.js
+++ Day-4_Loops/loops.js
@@ -117,12 +117,20 @@ const rightAngledTriangle = ()=>{
             console.log(i)
         }
     }
-    skipFunc();
+    // skipFunc();
 
 // . Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
+const breakFunc = ()=>{
+    for(let i=1; i<=10; i++){
+        // console.log(i)
+        if(i==7){
+            break;
+        }
+        console.log(i);
+    }
+}
 
-
-  
+breakFunc();
 
 
 
